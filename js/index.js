464f9c10f6f81f5ed73238f741ab12ee51e67304
f245 <f245@ivkhk.local>
2016-06-03 11:18:47 +0300

    Сделать валидацию логина и пароля при нажатии кнопки

---
+++ js/index.js
@@ -0,0 +1,9 @@
+alert( "Привет, Мир!" ) ;
+ document.getElementById('nav').innerHTML="Этот текст вывел JavaScript";
+ document.getElementById('button').ondblclick = function showMessage(){alert( 'Вы умрете через год!! ');}
+
+ function textAricle() {
+ document.getElementById('article').innerHTML="Это текст статьи...   :)";
+  document.getElementById('button').onclick = function showMessage(){alert( 'Привет!!' );}
+}
+
